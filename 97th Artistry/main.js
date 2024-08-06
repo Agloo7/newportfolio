c60dbd032580a501dfc5ac615e95544033f82fcb
Austin Agladima <129801717+Agloo7@users.noreply.github.com>
2024-08-06 04:59:49 +0200

    Add files via upload

---
+++ 97th Artistry/main.js
@@ -0,0 +1,17 @@
+var myHeading;
+myHeading = document.getElementById("my-heading");
+console.log(myHeading)
+console.log(typeof myHeading)
+var myPara;
+myPara = document.getElementById("my-para");
+console.log(myPara)
+console.log(typeof myPara)
+var a = 5
+a += 2
+console.log(a)
+myHeading.style.color = "#F4CCCC"
+myPara.style.color = "#000080"
+myPara.style.fontSize = "30pt"
+myHeading.style.fontSize = "80pt"
+myHeading.style.textAlign = "center"
+myPara.style.textAlign = "center"
